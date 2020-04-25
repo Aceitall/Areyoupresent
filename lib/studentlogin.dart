366b8273e06925854e2c586c461487effa06c4bf
Aceitall <loloicurmt@gmail.com>
2020-04-25 19:44:11 +0530

    I got the login for Faculty and Student working

--- lib/studentlogin.dart
+++ lib/studentlogin.dart
@@ -9,115 +9,124 @@ class StudentLogin extends StatefulWidget {
 }
 
 class _StudentLoginState extends State<StudentLogin> {
-
-  String _username, _password;
+  String _username, _password, passcode, type;
+  final db = Firestore.instance;
   final GlobalKey<FormState> _studentLoginKey = GlobalKey<FormState>();
-   
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Form(
-        key: _studentLoginKey,
-        child: SingleChildScrollView(
-        child: Stack(
-          children: <Widget>[
-            
-            Image.asset('assets/studentLogin.png',
-              height: 800,
-              width: 600,
-              fit: BoxFit.cover,),
-
-            Container(
-              margin: const EdgeInsets.only(top: 400.0, bottom: 100, left: 10, right: 10),
-              padding: const EdgeInsets.only(top: 10.0),
-              
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20.0),
-                color: Colors.transparent,
+        body: Form(
+            key: _studentLoginKey,
+            child: SingleChildScrollView(
+                child: Stack(
+              children: <Widget>[
+                Image.asset(
+                  'assets/studentLogin.png',
+                  height: 800,
+                  width: 600,
+                  fit: BoxFit.cover,
                 ),
-              
-               child: Column(
-                children: [
-                  TextFormField(
-                  validator: (input){
-                  if(input.isEmpty){
-                    return 'This field cannot be left empty';
-                    }
-                  },
-                  onSaved: (input) => _username = input,
-                  style: TextStyle(color: Color(0xff322F02), fontSize: 25),
-                  decoration: InputDecoration(
-                    labelText: 'Username',
-                    labelStyle: TextStyle(color: Colors.white),
-                    focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.white, width: 1.0),
+                Container(
+                    margin: const EdgeInsets.only(
+                        top: 400.0, bottom: 100, left: 10, right: 10),
+                    padding: const EdgeInsets.only(top: 10.0),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20.0),
+                      color: Colors.transparent,
                     ),
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.white, width: 1.0),
-                    ),
-                    fillColor: Colors.white
-                    ),
-                  ),
-            
-
-                  TextFormField(
-                    validator: (input){
-                      if(input.isEmpty){
-                        return 'This field cannot be left empty';
-                      }else if(input.length < 8){
-                        return 'Password too short. Please enter a valid Password';
-                      }
-                    },
-                    onSaved: (input) => _password = input,
-                    style: TextStyle(color: Color(0xff322F02), fontSize: 25),
-                    decoration: InputDecoration(
-                      labelText: 'Password',
-                      labelStyle: TextStyle(color: Colors.white),
-                          focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.white, width: 1.0),
+                    child: Column(children: [
+                      TextFormField(
+                        validator: (input) {
+                          if (input.isEmpty) {
+                            return 'This field cannot be left empty';
+                          }
+                        },
+                        onSaved: (input) => _username = input,
+                        style:
+                            TextStyle(color: Color(0xff322F02), fontSize: 25),
+                        decoration: InputDecoration(
+                            labelText: 'Username',
+                            labelStyle: TextStyle(color: Colors.white),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide:
+                                  BorderSide(color: Colors.white, width: 1.0),
+                            ),
+                            enabledBorder: OutlineInputBorder(
+                              borderSide:
+                                  BorderSide(color: Colors.white, width: 1.0),
+                            ),
+                            fillColor: Colors.white),
+                      ),
+                      TextFormField(
+                        validator: (input) {
+                          if (input.isEmpty) {
+                            return 'This field cannot be left empty';
+                          } else if (input.length < 8) {
+                            return 'Password too short. Please enter a valid Password';
+                          }
+                        },
+                        onSaved: (input) => _password = input,
+                        style:
+                            TextStyle(color: Color(0xff322F02), fontSize: 25),
+                        decoration: InputDecoration(
+                            labelText: 'Password',
+                            labelStyle: TextStyle(color: Colors.white),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide:
+                                  BorderSide(color: Colors.white, width: 1.0),
+                            ),
+                            enabledBorder: OutlineInputBorder(
+                              borderSide:
+                                  BorderSide(color: Colors.white, width: 1.0),
+                            ),
+                            fillColor: Colors.white),
+                        obscureText: true,
+                      ),
+                      ButtonTheme(
+                        minWidth: 200,
+                        height: 50,
+                        buttonColor: Color(0xff322F02),
+                        child: RaisedButton(
+                          onPressed: () {
+                            studentLogin();
+                          },
+                          child: Text(
+                            'Login',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontFamily: 'Sedan SC',
+                                fontSize: 25),
                           ),
-                        enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.white, width: 1.0),
-                          ),
-                          fillColor: Colors.white
-                    ),
-                    obscureText: true,
-                  ),
-            
-                  ButtonTheme(
-                    minWidth: 200,
-                    height: 50,
-                    buttonColor: Color(0xff322F02),
-                    
-                  child: 
-                  RaisedButton(
-                    onPressed: (){
-                      studentLogin();
-                      },
-                    child: Text('Login',  style: TextStyle(color: Colors.white, fontFamily: 'Sedan SC', fontSize: 25  ),),
-                    ),
-                  )
-                ]
-              )
-            )
-          ],
-        )
-      )
-    )
-    );
+                        ),
+                      )
+                    ]))
+              ],
+            ))));
   }
 
-Future <void> studentLogin() async {
-  final formState = _studentLoginKey.currentState;
-  if(formState.validate()){
-    formState.save();
-    try{
-      Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => StudentHome()));
-    }catch(e){
-      print(e.message);
+  Future<void> studentLogin() async {
+    final formState = _studentLoginKey.currentState;
+    if (formState.validate()) {
+      formState.save();
+
+      try {
+        DocumentSnapshot snapshot =
+            await db.collection('Users').document(_username).get();
+        passcode = snapshot.data['Password'];
+        type = snapshot.data['Type'];
+        String tp = "Student";
+        if (passcode == _password && type == tp) {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => StudentHome()));
+        } else if (passcode != _password) {
+          print("Incorrect password");
+        } else {
+          print("Incorrect User type");
+        }
+      }catch (e) {
+        print(e.message);
+      }
     }
-  }   
-}
+  }
 }
